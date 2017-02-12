c618a266584db9dabaa0035b947612e7fd8d121a
leonli87 <leon.li8787@gmail.com>
2017-02-11 17:00:18 -0800

    added

---
+++ Card.java
@@ -0,0 +1,41 @@
+package com.example.leon.bang;
+
+/**
+ * Created by Leon on 2/11/2017.
+ */
+
+public class Card {
+    private String rank;
+
+    private String suit;
+    private int value;
+
+    public String get_rank()
+    {
+        return rank;
+    }
+
+    public String get_suit()
+    {
+        return suit;
+    }
+
+    public int get_value()
+    {
+        return value;
+    }
+
+    public void print()
+    {
+        // TODO
+        // Need to be linked to GUI
+        System.out.println(rank + " of " + suit + "\n");
+    }
+
+    public card(String a_rank, String a_suit, int a_value)
+    {
+        rank = a_rank;
+        suit = a_suit;
+        value = a_value;
+    }
+}
